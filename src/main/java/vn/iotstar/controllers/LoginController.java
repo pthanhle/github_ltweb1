e67df39889dd9d25060f55e0ccaca0283829695a
pthanhle <22110228@student.hcmute.edu.vn>
2024-09-03 22:15:04 +0700

    first commit

---
+++ src/main/java/vn/iotstar/controllers/LoginController.java
@@ -0,0 +1,35 @@
+package vn.iotstar.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LoginController extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.setContentType("text/html");
+		resp.setCharacterEncoding("UTF-8");
+		req.setCharacterEncoding("UTF-8");
+		String uname = req.getParameter("uname");
+		String psw = req.getParameter("psw");
+		PrintWriter writer = resp.getWriter();
+		writer.append("<p>Username: " + uname + "</p>");
+		writer.append("<p>Password: " + psw + "</p>");
+	}
+}
